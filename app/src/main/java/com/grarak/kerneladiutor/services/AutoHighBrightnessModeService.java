ac7953677482f7bd700d793caa96f7ee6c2d6404
Yoinx <joseph.schubert@gmail.com>
2016-06-15 22:31:22 -0400

    HBM: Enable a lock option to persist overriding AutoHBM on manual toggle across scren on/off events
    Change-Id: I40cf4373219a2dacaa5e20d1fe3273df34330335

--- app/src/main/java/com/grarak/kerneladiutor/services/AutoHighBrightnessModeService.java
+++ app/src/main/java/com/grarak/kerneladiutor/services/AutoHighBrightnessModeService.java
@@ -145,7 +145,9 @@ public class AutoHighBrightnessModeService extends Service {
                 public void onReceive(Context context, android.content.Intent intent) {
                     String strAction = intent.getAction();
                     if (strAction.equals(android.content.Intent.ACTION_SCREEN_OFF)) {
-                        HBM_Manually_Toggled = false;
+                        if (!Screen.isScreenHBMLockActive(context)) {
+                            HBM_Manually_Toggled = false;
+                        }
                         if (Screen.isScreenAutoHBMActive(getApplicationContext())) {
                             LuxThresh = Screen.getAutoHBMThresh(getApplicationContext());
                             deactivateLightSensorRead();
@@ -153,7 +155,9 @@ public class AutoHighBrightnessModeService extends Service {
                     }
 
                     if (strAction.equals(android.content.Intent.ACTION_SCREEN_ON)) {
-                        HBM_Manually_Toggled = false;
+                        if (!Screen.isScreenHBMLockActive(context)) {
+                            HBM_Manually_Toggled = false;
+                        }
                         if (Screen.isScreenAutoHBMActive(getApplicationContext())) {
                             LuxThresh = Screen.getAutoHBMThresh(getApplicationContext());
                             // Delay 250ms to allow sensor to reactivate after doze.
